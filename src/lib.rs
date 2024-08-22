e41794abfc6463d367d5696254c0a12fccb03701
Jing Yu <jyqu@novonordisk.com>
2024-08-22 11:14:24 +0100

    add real test materials

--- src/lib.rs
+++ src/lib.rs
@@ -410,10 +410,10 @@ mod tests {
     
     #[test]
     fn args() {
-        let args = Args::parse_from(&["test", "-i", "test/test.vep.vcf.gz", "-t", "1", "-f", "test.yaml", "--fields", "CSQ"]);
+        let args = Args::parse_from(&["test", "-i", "test/test.vep.vcf.gz", "-t", "1", "-f", "test/test.yaml", "--fields", "CSQ"]);
         assert_eq!(args.input, "test/test.vep.vcf.gz");
         assert_eq!(args.threads, 1);
-        assert_eq!(args.filter, "test.yaml");
+        assert_eq!(args.filter, "test/test.yaml");
         assert_eq!(args.fields, vec!["CSQ".to_string()]);
     }
 }
