18edd6f955099a34e6218b6091f4caeb84f4527a
Jing Yu <jyqu@novonordisk.com>
2024-09-21 11:16:46 +0100

    bump version to 0.2.2

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod variant;
 mod utils;
 
 #[derive(Parser)]
-#[command(version = "0.2.1", about = "Read a (normalised) .vcf[.gz] and output tsv/json. CSQ-aware.", long_about = None)]
+#[command(version = "0.2.2", about = "Read a (normalised) .vcf[.gz] and output tsv/json. CSQ-aware.", long_about = None)]
 #[command(styles=get_styles())]
 pub struct Args {
     /// input .vcf[.gz] file, or ignore to read from stdin
