24f7045e09b59092e773fdede4de4ea05b3bc8cf
logust79 <logust@yahoo.com>
2024-12-02 10:35:57 +0000

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub mod error;
 pub mod parser;
 
 #[derive(Parser)]
-#[command(version = "0.2.5", about = "Read a (normalised) .vcf[.gz] and output tsv/json. CSQ-aware.", long_about = None)]
+#[command(version = "0.3.0", about = "Read a (normalised) .vcf[.gz] and output tsv/json. CSQ-aware.", long_about = None)]
 #[command(styles=get_styles())]
 pub struct Args {
     /// input .vcf[.gz] file, or ignore to read from stdin
